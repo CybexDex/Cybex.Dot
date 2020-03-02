afccb2003cf8a2ba739b9efaee44db63ccdff468
koofrank <koofranker@gmail.com>
2020-03-02 10:34:01 +0800

    Update to latest env

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps : [{
+    name   : "cybex-dot-dex",
+    script : "yarn start",
+    error_file : "/data/cybexDotDex/err.log",
+    out_file : "/data/cybexDotDex/out.log"
+  }]
+}
